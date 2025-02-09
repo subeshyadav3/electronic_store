07c12f476744f12091655c9a5dad7a1229b7223b
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-02-09 08:41:31 +0545

    .

--- server/index.js
+++ server/index.js
@@ -15,11 +15,12 @@ const { checkAuth } = require('./middlewares/checkAuthCustomer.js');
 
 // Middleware
 const corsOptions = {
-    // origin:'http://localhost:5173',
-    origin:  'https://frontendstore-five.vercel.app/', // Replace this with your frontend URL
+    // origin:'http://localhost:5173', // Uncomment this when testing locally
+    origin: 'https://frontendstore-five.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies to be sent
+    credentials: true, 
 };
+
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
