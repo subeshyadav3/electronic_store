c785258a4101266dfb42e2fb7540efbb3fa4a90e
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-02-09 08:29:25 +0545

    .

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getProducts, getProductById, addProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const checkAdmin = require('../middlewares/checkAdmin');
-// const {upload}=require('../index.js');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
